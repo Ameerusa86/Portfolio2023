5ca86a42ad60d6579009b0a62fa63c04d993a61e
ameer.hasan <engamermecha@gmail.com>
2023-04-15 17:38:38 -0400

    -

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -39,72 +39,26 @@ const Projects = () => {
             whileInView={fade}
           >
             {ProjectsData.map((w, index) => {
-              {
-                {
-                  /* return (
-                  <div key={index}>
-                    <Link
-                      to={w.site}
-                      target="_blank"
-                      className="work-link-group"
-                    >
-                      <div className="works-card">
-                        <div className="works-container">
-                          <div className="top-work">
-                            <FiFolder className="work-folder" />
-                            <div className="right">
-                              <Link
-                                className="work-git"
-                                to={w.gitlink}
-                                target="_blank"
-                              >
-                                <FiGithub />
-                              </Link>
-                              <Link
-                                className="work-link"
-                                to={w.site}
-                                target="_blank"
-                              >
-                                <IoOpenOutline />
-                              </Link>
-                            </div>
-                          </div>
-                          <div className="mid-work">
-                            <p className="work-title">{w.title}</p>
-                            <p className="work-desc">{w.desc}</p>
-                          </div>
-                          <div className="bottom-work">
-                            {w.tech.map((e, index) => {
-                              return <small key={index}>{e}</small>;
-                            })}
-                          </div>
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                ); */
-                }
-                return (
-                  <div className="myCard" key={index}>
-                    <div className="innerCard">
-                      <div className="frontSide">
-                        <h1 className="title">{w.title}</h1>
-                      </div>
-                      <div className="backSide">
-                        <p className="back_title">{w.tech}</p>
-                        <div className="icon_container">
-                          <Link to={w.gitlink}>
-                            <AiFillGithub className="icon" />
-                          </Link>
-                          <Link className="icon" to={w.site}>
-                            <SiNetlify />
-                          </Link>
-                        </div>
+              return (
+                <div className="myCard" key={index}>
+                  <div className="innerCard">
+                    <div className="frontSide">
+                      <h1 className="title">{w.title}</h1>
+                    </div>
+                    <div className="backSide">
+                      <p className="back_title">{w.tech}</p>
+                      <div className="icon_container">
+                        <Link to={w.gitlink}>
+                          <AiFillGithub className="icon" />
+                        </Link>
+                        <Link className="icon" to={w.site}>
+                          <SiNetlify />
+                        </Link>
                       </div>
                     </div>
                   </div>
-                );
-              }
+                </div>
+              );
             })}
           </motion.div>
         </div>
