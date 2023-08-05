eee571f4a1d3bc16775983e164e5274da274faab
Ameer Alhasnawi <46317379+Ameerusa86@users.noreply.github.com>
2023-08-05 17:30:42 -0400

    update

--- src/components/About.js
+++ src/components/About.js
@@ -30,18 +30,33 @@ const About = () => {
               className="about-content"
             >
               <p>
-                Hello! My name is Ameer and I enjoy creating things that live on
-                the internet. My interest in web development and Andriod app
-                development started back in 2020 when I decided to try out for
-                my school projects — turns out putting together a simple website
-                taught me a lot about HTML, CSS, & Java!{" "}
+                Greetings! I'm Ameer, a budding Fullstack Developer on an
+                exciting expedition through the ever-evolving landscape of
+                technology. My voyage began when I took my first steps into web
+                and Android app development back in 2020, captivated by the
+                prospect of bringing digital dreams to life. Starting with
+                humble school projects, I dabbled in the art of weaving together
+                HTML, CSS, and Java, sculpting simple ideas into functional
+                websites. This journey transformed into a passion, propelling me
+                to explore deeper into the world of coding and creation.
               </p>
               <br />
               <p>
                 {" "}
-                Nowadys I'm working on enhancing my skills in ReactJS and
-                MERN(MongoDB, ExpressJS, ReactJS & NodeJS) technologies. With my
-                experience I have what it takes to solve real world problems.
+                Today, armed with a strong foundation and an insatiable
+                curiosity, I'm immersed in the realm of ReactJS and the
+                versatile MERN stack — MongoDB, ExpressJS, ReactJS, NodeJS and
+                NextJS. As a junior fullstack developer, I relish the
+                opportunity to bridge the gap between back-end complexities and
+                front-end aesthetics, crafting seamless user experiences from
+                end to end. But it's not just about code for me. I'm driven by
+                the desire to solve real-world puzzles, to contribute
+                meaningfully to the digital tapestry that connects us all. Every
+                challenge I conquer and every project I bring to life is a step
+                forward in my journey as a junior fullstack developer. Join me
+                as I embrace the exhilarating challenges, celebrate the small
+                victories, and continue to refine my skills in the captivating
+                realm of technology.
               </p>
             </motion.div>
             <motion.div
